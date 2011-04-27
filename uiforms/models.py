7892eb198b600822884d814563aeaa251bc9aac7
Thomas Shaddox <wediddoit@gmail.com>
2011-04-27 15:36:57 -0500

    Fixed two mistakes in models.py.

--- uiforms/models.py
+++ uiforms/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 class UIForm(models.Model):
-    created_at = models.DateTimeField(datetime.now())
+    created_at = models.DateTimeField(default=datetime.now())
     owner = models.ForeignKey(User)
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
@@ -15,7 +15,7 @@ class UIFormField(models.Model):
         ('i', 'Integer'),
         )
     field_type = models.CharField(max_length=1, choices=FIELD_TYPE_CHOICES)
-    created_at = models.DateTimeField(datetime.now())
+    created_at = models.DateTimeField(default=datetime.now())
     label = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
     ui_form = models.ForeignKey(UIForm)
