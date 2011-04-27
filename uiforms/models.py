9f8144f1800b01480ae1ab8a76dba896c1d05c40
Thomas Shaddox <wediddoit@gmail.com>
2011-04-27 15:26:34 -0500

    (MIGRATION) Added the UIForm and UIFormField models.

--- uiforms/models.py
+++ uiforms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+class UIForm(models.Model):
+    created_at = models.DateTimeField(datetime.now())
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+
+class UIFormField(models.Model):
+    FIELD_TYPE_CHOICES = (
+        ('b', 'Boolean'),
+        ('i', 'Integer'),
+        )
+    field_type = models.CharField(max_length=1, choices=FIELD_TYPE_CHOICES)
+    created_at = models.DateTimeField(datetime.now())
+    label = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    ui_form = models.ForeignKey(UIForm)
